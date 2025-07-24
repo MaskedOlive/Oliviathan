3b56ecbe1ae8e3d9aaaa8371bff7450f7b0949a5
MaskedOlive <151477851+MaskedOlive@users.noreply.github.com>
2025-07-24 09:24:47 +0100

    Create utils.cpp

---
+++ src/utils.cpp
@@ -0,0 +1,4 @@
+#include "utils.h"
+
+// Currently, all functions in Utils are inline and implemented in the header for efficiency and ease of modification.
+// If you add more complex or non-inline functions in future, you can implement them here.
